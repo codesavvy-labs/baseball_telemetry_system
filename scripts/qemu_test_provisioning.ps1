8515974a78d334397b74227057c7b8f27eb8a4e5
TomH <tomhaley2015@gmail.com>
2026-05-18 09:17:52 -0400

    testing mkdir in pwsh script

--- scripts/qemu_test_provisioning.ps1
+++ scripts/qemu_test_provisioning.ps1
@@ -1,1 +1,2 @@
-mkdir -p ~/qemu-lab/{images,firmware,kernel,telemetry,logs,scripts,initramfsinitramfs/bin initramfs/proc initramfs/sys initramfs/dev}
+#mkdir -p ~/qemu-lab/{images,firmware,kernel,telemetry,logs,scripts,initramfsinitramfs/bin initramfs/proc initramfs/sys initramfs/dev}
+mkdir -p ~/qemu-lab
