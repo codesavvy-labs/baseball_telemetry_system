57e2c83c1b09ccd6255d11b8dc88db3863435b35
Tom Haley <tomhaley2015@gmail.com>
2026-06-11 16:03:54 -0400

    Validate docker image config

--- scripts/validate-build-env.sh
+++ scripts/validate-build-env.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
 python3 --version
 cargo --version
-cmake --version
-clang++ --version
-gcc --version
+cmake --version | head -n 1
+clang --version | head -n 1
+clang-tidy --version | head -n 1
+gcc --version | head -n 1
 dot -V
-qemu-system-x86_64 --version
-qemu-system-arm --version
-qemu-system-aarch64 --version
-qemu-img --version
-clang-tidy --version
-
+qemu-system-x86_64 --version | head -n 1
+qemu-img --version | head -n 1
+ls -l /usr/share/OVMF/OVMF_CODE_4M.fd
+ls -l /usr/share/OVMF/OVMF_VARS_4M.fd
+ls -l /bin/busybox
 echo "Toolchain validation passed."
