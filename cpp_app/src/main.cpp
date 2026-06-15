93b135c81d55ae1e5966c04b71ad456728c0276a
Tom Haley <tomhaley2015@gmail.com>
2026-06-14 20:32:17 -0400

    See of only app build triggers

--- cpp_app/src/main.cpp
+++ cpp_app/src/main.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "baseball telemetry cpp stub\n";
+    std::cout << "This is a simple C++ application for baseball telemetry.\n";
+    std::cout << "It is intended to be a stub for the actual implementation.\n";
+    std::cout << "The actual implementation will be added later.\n";
+    
     return 0;
 }
